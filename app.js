b7d043126b143ef3041fcfdeee7a5fdf878fa243
Modestas <modestask97@gmail.com>
2018-04-15 19:51:58 +0100

    app.js layout

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // main react component goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+
+// import './scss/style.scss';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Movie  Reviews</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
